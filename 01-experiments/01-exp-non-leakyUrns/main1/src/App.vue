fc4af5467ba2ac91a13d644d2fe3ec91952f4e6c
Erik Zeiner <47943924+ErikZeiner@users.noreply.github.com>
2026-01-30 23:15:23 +0100

    add a main version of Exp1

---
+++ 01-experiments/01-exp-non-leakyUrns/main1/src/App.vue
@@ -0,0 +1,434 @@
+<template>
+  <Experiment title="ChronoCause">
+
+    <InstructionScreen :title="'Welcome'">
+      Hi, thanks for taking part!
+      In this experiment, you will be introduced to a simple game.
+      Then, you will see a few rounds of the game being played and you will be asked to make some judgements.
+      <div class="pool-ball"></div>
+    </InstructionScreen>
+
+    <InstructionScreen :titles="'Game Rules'">
+      <p>
+        In the game, the player sees these two urns, which each contain both <b>solid</b> and <b>striped</b> balls.
+        There are always the same 10 balls in each urn: 5 of the balls are solid, and 5 are striped.
+      </p>
+
+      <div class="urns">
+        <Urn
+            firstColor='red'
+            :firstType='getType("red")'
+            :firstCount=5
+            secondColor='blue'
+            :secondType='getType("blue")'
+            :secondCount=5
+
+        />
+        <Urn
+            firstColor='yellow'
+            :firstType='getType("yellow")'
+            :firstCount=5
+            secondColor='green'
+            :secondType='getType("green")'
+            :secondCount=5
+        />
+      </div>
+    </InstructionScreen>
+
+    <InstructionScreen :titles="'Game Rules'">
+      <p>
+        In the game, the player sees these two urns, which each contain both <b>solid</b> and <b>striped</b> balls.
+        There are always the same 10 balls in each urn: 5 of the balls are solid, and 5 are striped.
+      </p>
+
+      <div class="urns">
+        <Urn
+            firstColor='red'
+            :firstType='getType("red")'
+            :firstCount=5
+            secondColor='blue'
+            :secondType='getType("blue")'
+            :secondCount=5
+
+        />
+        <Urn
+            firstColor='yellow'
+            :firstType='getType("yellow")'
+            :firstCount=5
+            secondColor='green'
+            :secondType='getType("green")'
+            :secondCount=5
+        />
+      </div>
+      <p>
+        When the player presses the button, a ball is released from each urn.
+        The player wins just in case
+        <b>{{
+            structure == "conjunctive" ? "both" : "at least one"
+          }}</b> {{
+          structure == "conjunctive" ? "balls are" : "ball is"
+        }}
+        <b>solid</b>.
+        <br/>
+        Here are the possible outcomes:
+      </p>
+      <div class="outcomes">
+        <div class="col">
+          <Ball color="red" :type='getType("red")'/>
+          <Ball color="blue" :type='getType("blue")'/>
+          <Ball color="red" :type='getType("red")'/>
+          <Ball color="blue" :type='getType("blue")'/>
+        </div>
+        <div class="col">
+          <Ball color="yellow" :type='getType("yellow")'/>
+          <Ball color="yellow" :type='getType("yellow")'/>
+          <Ball color="green" :type='getType("green")'/>
+          <Ball color="green" :type='getType("green")'/>
+        </div>
+        <div class="col">
+          <p>WIN</p>
+          <p>{{ structure == "conjunctive" ? "LOSE" : "WIN" }}</p>
+          <p>{{ structure == "conjunctive" ? "LOSE" : "WIN" }}</p>
+          <p>LOSE</p>
+        </div>
+      </div>
+    </InstructionScreen>
+
+
+    <template v-for="(trial, i) in comprehension">
+      <Screen>
+        <Slide>
+          <p>
+            Remember, the player wins just in case in case
+            <b>{{ structure == "conjunctive" ? "both" : "at least one" }}</b>
+            {{ structure == "conjunctive" ? "balls are" : "ball is" }}
+            <b>solid</b>.
+          </p>
+
+          To make sure you understand, please select whether a player would win or lose when the following balls are
+          released:
+
+          <p class="outcomeBalls">
+            <Ball class="col" :color="getColorLeftUrn(trial.earlyBall)" :type="trial.earlyBall"/>
+            <Ball clas="col" :color="getColorRightUrn(trial.lateBall)" :type="trial.lateBall"/>
+          </p>
+
+          <ForcedChoiceInput
+              :response.sync="$magpie.measurements.comprehensionResponse"
+              :options="['win', 'lose']"
+              @update:response="saveComprehensionResponse($magpie.measurements.comprehensionResponse,trial.correctResponse)"/>
+
+          <Record
+              :data="{
+              trialType : 'comprehension-1',
+              trialNr : i+1,
+              comprehensionCorrectResponse: trial.correctResponse,
+              earlyBall : trial.earlyBall,
+              lateBall : trial.lateBall,
+              comprehensionResponse : $magpie.measurements.comprehensionResponse,
+              structure : structure
+            }"
+          />
+        </Slide>
+      </Screen>
+    </template>
+
+    <InstructionScreen>
+      <p v-if="!comprehensionFailed">Great, you understood the task! Let’s begin.</p>
+
+      <p v-if="comprehensionFailed"> Oops! You made a mistake.<br/>
+        Remember, the player wins just in case
+        <b>{{
+            structure == "conjunctive" ? "both" : "at least one"
+          }}</b> {{
+          structure == "conjunctive" ? "balls are" : "ball is"
+        }}
+        <b>solid</b>.
+        <br/>
+        Here are the possible outcomes:
+      </p>
+      <div v-if="comprehensionFailed" class="outcomes">
+        <div class="col">
+          <Ball color="red" :type='getType("red")'/>
+          <Ball color="blue" :type='getType("blue")'/>
+          <Ball color="red" :type='getType("red")'/>
+          <Ball color="blue" :type='getType("blue")'/>
+        </div>
+        <div class="col">
+          <Ball color="yellow" :type='getType("yellow")'/>
+          <Ball color="yellow" :type='getType("yellow")'/>
+          <Ball color="green" :type='getType("green")'/>
+          <Ball color="green" :type='getType("green")'/>
+        </div>
+        <div class="col">
+          <p>WIN</p>
+          <p>{{ structure == "conjunctive" ? "LOSE" : "WIN" }}</p>
+          <p>{{ structure == "conjunctive" ? "LOSE" : "WIN" }}</p>
+          <p>LOSE</p>
+        </div>
+      </div>
+      <p v-if="comprehensionFailed">Let’s try again!
+      </p>
+    </InstructionScreen>
+
+    <template v-if="comprehensionFailed" v-for="(trial, i) in comprehension">
+      <Screen>
+        <Slide>
+          <p>
+            Remember, the player wins just in case in case
+            <b>{{ structure == "conjunctive" ? "both" : "at least one" }}</b>
+            {{ structure == "conjunctive" ? "balls are" : "ball is" }}
+            <b>solid</b>.
+          </p>
+
+          To make sure you understand, please select whether a player would win or lose when the following balls are
+          released:
+
+          <p class="outcomeBalls">
+            <Ball class="col" :color="getColorLeftUrn(trial.earlyBall)" :type="trial.earlyBall"/>
+            <Ball clas="col" :color="getColorRightUrn(trial.lateBall)" :type="trial.lateBall"/>
+          </p>
+
+          <ForcedChoiceInput
+              :response.sync="$magpie.measurements.comprehensionResponse"
+              :options="['win', 'lose']"
+              @update:response="$magpie.saveAndNextScreen();"/>
+
+          <Record
+              :data="{
+              trialType : 'comprehension-2',
+              trialNr : i+1,
+              comprehensionCorrectResponse: trial.correctResponse,
+              earlyBall : trial.earlyBall,
+              lateBall : trial.lateBall,
+              comprehensionResponse : $magpie.measurements.comprehensionResponse,
+              structure : structure
+            }"
+          />
+        </Slide>
+      </Screen>
+    </template>
+
+    <InstructionScreen :title="'Instructions'">
+      <p>
+        In this experiment, Alice will be playing the game.
+        You will see a few rounds, and after each round, you will be asked to judge several statements.
+      </p>
+      <p>
+        <b>Pay close attention</b> to when the balls are released from either of the two urns, as they may be released
+        at different times.
+      </p>
+      <p>Let’s practice this first!</p>
+    </InstructionScreen>
+
+    <template v-for="(trial, i) of training_trials">
+      <TrialScreen trialType="training" :trial="trial" :index="i" :length="training_trials.length" :getType="getType"
+                   :getDelay="getDelay" :which_urn_prompted_first="which_urn_prompted_first"/>
+    </template>
+
+    <InstructionScreen :title="'Instructions'">
+      <p>Great! You are now ready to start the main experiment.</p>
+    </InstructionScreen>
+
+    <template v-for="(trial, i) of main_trials">
+      <TrialScreen trialType="critical" :trial="trial" :index="i" :length="main_trials.length" :getType="getType"
+                   :getDelay="getDelay" :which_urn_prompted_first="which_urn_prompted_first"/>
+    </template>
+
+    <PostTestScreen/>
+    <SubmitResultsScreen/>
+  </Experiment>
+</template>
+
+<script>
+import _ from "lodash";
+
+import UrnGame from "../../../00-customComponents/UrnGame.vue";
+import Urn from "../../../00-customComponents/Urn.vue";
+import Ball from "../../../00-customComponents/Ball.vue";
+
+import TrialScreen from "./TrialScreen.vue";
+
+import training_trials_all from "../trials/training_trials.csv";
+import main_trials_all from "../trials/main_trials.csv";
+import comprehension_all from "../trials/comprehension.csv";
+
+
+const structure = _.sample(["conjunctive", "disjunctive"]);
+// console.log('structure ', structure);
+const which_urn_prompted_first = _.sample(["left", "right"]);
+// console.log('which_urn_prompted_first', which_urn_prompted_first);
+let main_trials = _.shuffle(_.filter(main_trials_all, function (i) {
+  return i.structure == structure;
+}));
+
+main_trials.forEach(trial => {
+  if (trial['delay'] == 'simult') {
+    trial['delayedUrn'] = 'none';
+  } else {
+    trial['delayedUrn'] = _.sample(['left', 'right']);
+  }
+});
+
+// add attention checks
+main_trials[2]['attentionCheck'] = true
+main_trials[5]['attentionCheck'] = true
+main_trials[8]['attentionCheck'] = true
+main_trials[11]['attentionCheck'] = true
+
+const training_trials = _.filter(training_trials_all, function (i) {
+  return i.structure == structure;
+});
+
+training_trials.forEach(trial => {
+  if (trial['delay'] == 'simult') {
+    trial['delayedUrn'] = 'none';
+  } else {
+    trial['delayedUrn'] = _.sample(['left', 'right']);
+  }
+});
+
+const comprehension = _.shuffle(_.filter(comprehension_all, function (i) {
+  return i.structure == structure;
+}));
+
+console.log('training trials', JSON.parse(JSON.stringify(training_trials)));
+console.log('main trials', JSON.parse(JSON.stringify(main_trials)));
+
+export default {
+  name: "App",
+  components: {TrialScreen, Urn, UrnGame, Ball},
+  data() {
+    return {
+      structure: structure,
+      which_urn_prompted_first: which_urn_prompted_first,
+      main_trials: main_trials,
+      training_trials: training_trials,
+      comprehension: comprehension,
+      comprehensionFailed: false
+    };
+  },
+  methods: {
+    getType(color) {
+      let type;
+      switch (color) {
+        case 'red':
+          type = 'solid';
+          break;
+        case 'yellow':
+          type = 'solid';
+          break;
+        case 'blue':
+          type = 'stripe';
+          break;
+        case 'green':
+          type = 'stripe';
+          break;
+        default:
+          type = 'solid';
+      }
+      return type;
+    },
+    getDelay(name) {
+      const base_delay = 1000;
+      let delay;
+      switch (name) {
+        case 'base':
+          delay = base_delay;
+          break;
+        case 'short':
+          delay = base_delay + 2000;
+          break;
+        case 'medium':
+          delay = base_delay + 5000;
+          break;
+        case 'long':
+          delay = base_delay + 8000;
+          break;
+        default:
+          delay = base_delay;
+      }
+      return delay;
+    },
+    getColorLeftUrn(type) {
+      if (type === "solid") {
+        return "red"
+      } else {
+        return "blue"
+      }
+    },
+    getColorRightUrn(type) {
+      if (type === "solid") {
+        return "yellow"
+      } else {
+        return "green"
+      }
+    },
+    saveComprehensionResponse: function (response, correctResponse) {
+      this.comprehensionFailed = (this.comprehensionFailed || !(response == correctResponse));
+      $magpie.saveAndNextScreen();
+    }
+  },
+  computed: {
+    _() {
+      return _;
+    }
+  }
+};
+</script>
+
+<style>
+.outcomes {
+  width: 100%;
+  justify-content: center;
+  display: grid;
+  grid-template-columns: 50px 50px 50px;
+}
+
+.outcomes .col {
+  display: grid;
+  align-items: center;
+  gap: 10px;
+  grid-template-rows: 50px 50px 50px 50px;
+}
+
+.outcomeBalls {
+  justify-content: center;
+  display: grid;
+  grid-template-columns: 50px 50px;
+  gap: 10px;
+  padding-top: 40px;
+  padding-bottom: 40px;
+}
+
+.outcomeBalls .col {
+  display: grid;
+  align-items: center;
+
+}
+
+.urns {
+  width: 100%;
+  justify-content: center;
+  display: grid;
+  grid-template-columns: auto auto;
+  gap: 50px;
+}
+
+.red {
+  color: #B22222;
+}
+
+.yellow {
+  color: #EEB14F;
+}
+
+.green {
+  color: #3C902B;
+}
+
+.blue {
+  color: #3A4FB4;
+}
+
+</style>
---
+++ 01-experiments/01-exp-non-leakyUrns/main1/src/TrialScreen.vue
@@ -0,0 +1,247 @@
+<!--
+Screen with multiple slides representing one trial (both main and training)
+
+Parameters:
+  'trialType' - description of the trial in the output csv
+  'trial' - current trial object
+  'index' - index of current trial
+  'length' - number of either main or training trials
+  'getType' - used to pass the getType function defined in App.vue
+  'getDelay' - used to pass the getDelay function defined in App.vue
+  'which_urn_prompted_first' - used to pass the which_urn_prompted_first value
+
+Example usage:
+  <template v-for="(trial, i) of training_trials">
+    <TrialScreen trialType="training" :trial="trial" :index="i" :length="training_trials.length" :getType="getType" :getDelay="getDelay"/>
+  </template>
+-->
+
+<script setup>
+
+import UrnGame from "../../../00-customComponents/UrnGame.vue";
+
+const props = defineProps({
+  trialType: String,
+  trial: Object,
+  index: Number,
+  length: Number,
+  getType: Function,
+  getDelay: Function,
+  which_urn_prompted_first: String
+});
+
+function saveAndNextScreenTimeLog() {
+  $magpie.measurements.submitClicked = Date.now();
+  $magpie.saveAndNextScreen();
+}
+
+function saveAndNextSlideTimeLog() {
+  $magpie.measurements.submitClicked = Date.now();
+  $magpie.saveMeasurements();
+  $magpie.nextSlide();
+}
+
+function getColorLeftUrn(type) {
+  if (type === "solid") {
+    return "red"
+  } else {
+    return "blue"
+  }
+}
+
+function getColorRightUrn(type) {
+  if (type === "solid") {
+    return "yellow"
+  } else {
+    return "green"
+  }
+}
+
+function getLeftColor() {
+  return props.trial.delayedUrn === 'left' ? getColorLeftUrn(props.trial.lateBall) : getColorLeftUrn(props.trial.earlyBall)
+}
+
+function getRightColor() {
+  return props.trial.delayedUrn === 'left' ? getColorRightUrn(props.trial.earlyBall) : getColorRightUrn(props.trial.lateBall)
+}
+
+function getLeftType() {
+  return props.trial.delayedUrn === 'left' ? props.trial.lateBall : props.trial.earlyBall;
+}
+
+function getRightType() {
+  return props.trial.delayedUrn === 'left' ? props.trial.earlyBall : props.trial.lateBall;
+}
+
+function getLeftTiming() {
+  return props.trial.delayedUrn === 'left' ? props.getDelay(props.trial.delay) : props.getDelay('base');
+}
+
+function getRightTiming() {
+  return props.trial.delayedUrn === 'left' ? props.getDelay('base') : props.getDelay(props.trial.delay);
+}
+
+
+</script>
+
+<template>
+  <Screen :progress="props.index/props.length">
+    <Slide>
+      Press the button to start a new round of Alice's game:
+      <UrnGame
+          firstColorLeft='red'
+          :firstTypeLeft='getType("red")'
+          secondColorLeft='blue'
+          :secondTypeLeft='getType("blue")'
+          firstColorRight='yellow'
+          :firstTypeRight='getType("yellow")'
+          secondColorRight='green'
+          :secondTypeRight='getType("green")'
+          :outputColorLeft="getLeftColor()"
+          :outputColorRight="getRightColor()"
+          :outputTypeLeft="getLeftType()"
+          :outputTypeRight="getRightType()"
+          :timingLeft="getLeftTiming()"
+          :timingRight="getRightTiming()"
+          :enabled="true"
+          :delayedUrn="props.trial.delayedUrn"
+      />
+    </Slide>
+
+    <Slide v-if="props.trial.attentionCheck">
+      Press the button to start a new round of Alice's game:
+      <UrnGame
+          firstColorLeft='red'
+          :firstTypeLeft='getType("red")'
+          secondColorLeft='blue'
+          :secondTypeLeft='getType("blue")'
+          firstColorRight='yellow'
+          :firstTypeRight='getType("yellow")'
+          secondColorRight='green'
+          :secondTypeRight='getType("green")'
+          :outputColorLeft="getLeftColor()"
+          :outputColorRight="getRightColor()"
+          :outputTypeLeft="getLeftType()"
+          :outputTypeRight="getRightType()"
+          :timingLeft="getLeftTiming()"
+          :timingRight="getRightTiming()"
+          :enabled="false"
+          :delayedUrn="props.trial.delayedUrn"
+      />
+
+      Alice <b>{{ props.trial.gameOutcome === "win" ? "won" : "lost" }}</b> the game.
+      <br/>
+
+      <p>
+        Which ball was released first?
+        <MultipleChoiceInput
+            :response.sync="$magpie.measurements.attentionResponse"
+            :options="[
+                'The ' + getLeftColor() + ' ball',
+                'The ' + getRightColor() + ' ball',
+                'Both balls were released at the same time']"/>
+
+        <button v-if="$magpie.measurements.attentionResponse" @click="saveAndNextSlideTimeLog">Submit</button>
+      </p>
+
+      <Record
+          :data="{
+              trialType : props.trialType + '-attention',
+              trialNr : props.index + 1,
+              structure : props.trial.structure,
+              earlyBall : props.trial.earlyBall,
+              lateBall : props.trial.lateBall,
+              gameOutcome : props.trial.gameOutcome,
+              delay : props.trial.delay,
+              delayedUrn : props.trial.delayedUrn,
+              attentionCheck : props.trial.attentionCheck,
+              attentionResponse : $magpie.measurements.attentionResponse,
+              attentionCorrectResponse : props.trial.delayedUrn == 'none' ? 'Both balls were released at the same time':
+              (props.trial.delayedUrn =='left' ? 'The ' + getRightColor() + ' ball' : 'The ' + getLeftColor() + ' ball'),
+              beginClicked : $magpie.measurements.beginClicked,
+              submitClicked : $magpie.measurements.submitClicked
+            }"
+      />
+    </Slide>
+
+    <Slide>
+      Press the button to start a new round of Alice's game:
+      <UrnGame
+          firstColorLeft='red'
+          :firstTypeLeft='getType("red")'
+          secondColorLeft='blue'
+          :secondTypeLeft='getType("blue")'
+          firstColorRight='yellow'
+          :firstTypeRight='getType("yellow")'
+          secondColorRight='green'
+          :secondTypeRight='getType("green")'
+          :outputColorLeft="getLeftColor()"
+          :outputColorRight="getRightColor()"
+          :outputTypeLeft="getLeftType()"
+          :outputTypeRight="getRightType()"
+          :timingLeft="getLeftTiming()"
+          :timingRight="getRightTiming()"
+          :enabled="false"
+          :delayedUrn="props.trial.delayedUrn"
+      />
+
+      Alice <b>{{ props.trial.gameOutcome === "win" ? "won" : "lost" }}</b> the game.
+      <br/>
+
+      <p>
+        Do you agree with the following statements?
+      </p>
+
+      <p>
+        <b>Getting a {{ props.which_urn_prompted_first === "left" ? getLeftColor() : getRightColor() }}
+          ball caused Alice to
+          {{
+            props.trial.gameOutcome
+          }}.</b>
+      </p>
+      <RatingInput
+          left="strongly disagree"
+          right="strongly agree"
+          :response.sync="$magpie.measurements.responseLeft"
+      />
+
+      <p>
+        <b>Getting a {{ props.which_urn_prompted_first === "left" ? getRightColor() : getLeftColor() }}
+          ball caused Alice to
+          {{ props.trial.gameOutcome }}.</b>
+      </p>
+      <RatingInput
+          left="strongly disagree"
+          right="strongly agree"
+          :response.sync="$magpie.measurements.responseRight"
+
+      />
+      <p v-if="$magpie.measurements.responseLeft > 0 && $magpie.measurements.responseRight > 0">
+        <button @click="saveAndNextScreenTimeLog">Submit</button>
+      </p>
+
+      <Record
+          :data="{
+              trialType : props.trialType,
+              trialNr : props.index + 1,
+              structure:props.trial.structure,
+              earlyBall : props.trial.earlyBall,
+              lateBall : props.trial.lateBall,
+              gameOutcome: props.trial.gameOutcome,
+              delay: props.trial.delay,
+              delayedUrn: props.trial.delayedUrn,
+              attentionCheck : props.trial.attentionCheck,
+              whichUrnPromptedFirst : which_urn_prompted_first,
+              responseLeft: $magpie.measurements.responseLeft,
+              responseRight: $magpie.measurements.responseRight,
+              beginClicked: $magpie.measurements.beginClicked,
+              submitClicked: $magpie.measurements.submitClicked
+            }"
+      />
+    </Slide>
+  </Screen>
+</template>
+
+<style scoped>
+
+</style>
